786d629ac53f9f6d033d2674a083fbc747e36d46
richard-graham <richardcolingraham@gmail.com>
2019-04-01 23:23:26 +1300

    added basic structure

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,6 +1,21 @@
 import React from 'react'
+import { Grid } from '@material-ui/core/'
+import Directory from './Directory';
+import InfoPanel from './InfoPanel'
+
+const styles  = {
+  Paper: {
+    padding: 20,
+    margin: 10
+  }
+}
 
 export default props =>
-  <div>
-    Hello from Dashboard
-  </div>
+  <Grid container>
+    <Grid item sm>
+      <Directory styles={styles} />
+    </Grid>
+    <Grid item sm>
+      <InfoPanel styles={styles} />
+    </Grid>
+  </Grid>
